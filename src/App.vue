75b7d0c431e05b8126b4d63198bd5025e62950a9
Aispuro Butchershop <maniak@MacBook-Pro-de-Maniak.local>
2023-05-25 09:18:52 -0700

    Return to the last stable version

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-  import CompositionList from './components/composition/List.vue';
-  import OptionsList from './components/options/List.vue';
+  import List from './components/composition/List.vue';
 </script> 
 
 <template>
-  <header>
-    <h1>This is the list of posts</h1>
-  </header>
   <main>
-    <OptionsList/>
+    <List/>
   </main>
 </template>
